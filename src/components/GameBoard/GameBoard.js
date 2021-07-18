3f5f00e088c874d20fc29878714ed820786782eb
DaKlock11 <macbook@Macbooks-MacBook-Pro.local>
2021-07-18 17:18:34 -0400

    FFoundation setup

---
+++ src/components/GameBoard/GameBoard.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import './GameBoard.css';
+
+const DECK_OF_CARDS_API_ENDPOINT = 'https://deckofcardsapi.com/api/deck/new/';
+
+const GameBoard = () => {
+    const [apiState, setApiState] = useState(null);
+    useEffect(() => {
+        fetch(DECK_OF_CARDS_API_ENDPOINT).then(resp => {
+            return resp.json();
+        }).then( json => {
+            setApiState(json);
+        })
+    }, []);
+
+    return (
+        <div>{JSON.stringify(apiState, null, 4)}</div>
+    )
+};
+
+export default GameBoard;
